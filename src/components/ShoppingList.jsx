f54792f638ebdb70ed694504f95ce20d9bf9e465
Julia Çam <barbarbetsy@googlemail.com>
2019-11-18 10:54:16 +0100

    get rid of empty shoppinglist items

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -5,11 +5,13 @@ class ShoppingList extends React.Component {
   render() {
     const ingredientList = Object.keys(this.props.ingredients);
 
-    const listItems = ingredientList.map(item => (
-      <li key={item}>
-        {item}: {this.props.ingredients[item]}
-      </li>
-    ));
+    const listItems = ingredientList.map(item =>
+      this.props.ingredients[item] === 0 ? null : (
+        <li key={item}>
+          {item}: {this.props.ingredients[item]}
+        </li>
+      )
+    );
 
     return (
       <div className="zettel">
